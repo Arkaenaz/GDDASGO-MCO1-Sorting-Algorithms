3a65cfa4c4428076bce45ad9f13ec77d32c45395
Arkaenaz <flooper2234@gmail.com>
2023-01-28 23:24:31 +0800

    hi

---
+++ generateData.h
@@ -0,0 +1,19 @@
+/****************************************************
+REFERENCE
+
+Author:
+Link:
+****************************************************/
+
+
+/****************************************************
+YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
+****************************************************/
+
+
+#include <stdlib.h> 
+#include <time.h> 
+
+void generateData (int A[], int n) {   
+    //	your code here
+}
---
+++ main.c
@@ -0,0 +1,24 @@
+/****************************************************
+Group: 		
+Section:	
+Members:	
+****************************************************/
+
+#include <stdio.h>
+#include <time.h>
+#include "bubbleSort.h"
+#include "insertionSort.h"
+#include "selectionSort.h"
+#include "mergeSort.h"
+#include "sort5.h"
+#include "sort6.h"
+#include "generateData.h"
+#include "timer.h"
+
+
+int main() {
+	
+	//	your code here
+	
+	return 0;
+}

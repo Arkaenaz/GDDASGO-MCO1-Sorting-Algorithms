adb61e6a31c62ec425a8991d0c895428bb36bc27
Arkaenaz <flooper2234@gmail.com>
2023-01-31 21:18:35 +0800

    Added srand to randomize the values even more

--- generateData.h
+++ generateData.h
@@ -17,6 +17,9 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 void generateData (int A[], int n) {   
     //	your code here
     int i;
+
+    srand(time(NULL));
+
     for (i=0;i<n;i++) {
         A[i] = rand();
     }

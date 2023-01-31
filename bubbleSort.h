0ef977671b41a73125a381ca14b84358f8d940bb
Arkaenaz <flooper2234@gmail.com>
2023-02-01 04:24:58 +0800

    bubble sort done

--- bubbleSort.h
+++ bubbleSort.h
@@ -10,6 +10,12 @@ Link:
 	You may declare additional functions here
 */
 
+void swapBubble (int *nValue1, int *nValue2) {
+	int nTemp;
+    nTemp = *nValue1;
+    *nValue1 = *nValue2;
+    *nValue2 = nTemp;
+}
 
 /****************************************************
 YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
@@ -25,4 +31,15 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 */
 void bubbleSort(int A[], int n, double *dCounter) {
 	//	your code here
+    int i, j;
+
+	for(i = 0; i < n; i++) {
+		for(j = (n-1); j >= i+1; j--) {
+        	if(A[j] < A[j-1]) {
+				*dCounter += 1;
+            	swapBubble(&A[j], &A[j-1]);
+			}
+      	}
+   }
 }
+

8f7469338ca83c4091647eabafc6ac5d7ae3b792
Kirvvs <jb.ursua13@gmail.com>
2023-01-31 22:09:10 +0800

    selectionSort & heapSort finalized

--- selectionSort.h
+++ selectionSort.h
@@ -16,7 +16,13 @@ Link: https://www.programiz.com/dsa/selection-sort
 	@param int *nValue1     value #1
 	@param int *nValue2     value #2
 */
-void swap(int *nValue1, int *nValue2);
+void swap(int *nValue1, int *nValue2)
+{
+    int temp;
+    temp = nValue1;
+    nValue1 = nValue2;
+    nValue2 = temp;
+}
 
 /****************************************************
 YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
@@ -29,4 +35,18 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 	@param int n			size of the array to be sorted
 	@param double *dCounter	counter variable for critical parts of the code
 */
-void selectionSort(int A[], int n, double *dCounter);
+void selectionSort(int A[], int n, double *dCounter)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int nMinimum = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] < A[nMinimum])
+                nMinimum = j;
+        }
+
+        swap(&A[i], &A[nMinimum]);
+    }
+}
--- sort5.h
+++ sort5.h
@@ -9,9 +9,41 @@ Link:https://www.programiz.com/dsa/heap-sort
 /* 
 	You may declare additional functions here
 */
-void swap(int *nNum1, int *nNum2);
 
-void heapify(int arr[], int n, int i);
+/*
+	Swaps two(2) array indexes with one another
+
+	@param int *nValue1     value #1
+	@param int *nValue2     value #2
+*/
+void swap(int *nNum1, int *nNum2){
+    int temp = *nNum1;
+    *nNum1 = *nNum2;
+    *nNum2 = temp;
+}
+
+/*
+	This function does the tree node comparison between parent and child nodes.
+
+	@param int arr[] 		array to be sorted
+	@param int n			size of the array to be sorted
+	@param int i            array index
+	@param double *dCounter	counter variable for critical parts of the code
+*/
+void heapify(int arr[], int n, int i){
+    int small, left, right;
+    do{
+        small = i;
+        left  = 2 * i + 1;
+        right = 2 * i + 2;
+
+        if(left < n && arr[left] < arr[small]) small = left;
+        if(right < n && arr[right] < arr[small]) small = right;
+
+        if(small != i) swap(&arr[i], &arr[small]);
+        
+    }while(small != i);
+}
 
 
 /****************************************************
@@ -26,4 +58,9 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 	@param int n			size of the array to be sorted
 	@param double *dCounter	counter variable for critical parts of the code
 */
-void sort5(int A[], int n, double *dCounter);
+void sort5(int A[], int n, double *dCounter){
+
+    for(int i = n/2 - 1; i >= 0; i--){
+        heapify(A, n, i);
+    }
+}

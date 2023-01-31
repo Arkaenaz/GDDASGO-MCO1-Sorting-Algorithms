1ce29d57368da3a0d1dc1f7fe09d334bcf2b5e87
Kirvvs <jb.ursua13@gmail.com>
2023-01-31 18:38:31 +0800

    selectionSort commit

--- selectionSort.h
+++ selectionSort.h
@@ -5,16 +5,22 @@ Author:
 Link:
 ****************************************************/
 
-
-/* 
+/*
 	You may declare additional functions here
 */
 
 
+/*
+	Swaps two(2) array indexes with one another
+
+	@param int *nValue1     value #1
+	@param int *nValue2     value #2
+*/
+void swap(int *nValue1, int *nValue2);
+
 /****************************************************
 YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 ****************************************************/
-
 
 /*
 	Sorts the array A using selection sorting algorithm.
@@ -23,6 +29,4 @@ YOU ARE NOT ALLOWED TO MODIFY THE FUNCTION PROTOTYPES
 	@param int n			size of the array to be sorted
 	@param double *dCounter	counter variable for critical parts of the code
 */
-void selectionSort(int A[], int n, double *dCounter) {
-	//	your code here
-}
+void selectionSort(int A[], int n, double *dCounter);
